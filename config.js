006abb7846108578c42374a311d8a3a73f09dad5
fanta <fms294@nyu.edu>
2020-09-29 21:57:24 -0400

    Added oauth2, using facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-87554-09864-78553',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook':{
+        clientId :'639693936694502',
+        clientSecret: 'eb2abaaa311870db6558f7d90c9df96a'
+    }
 }
